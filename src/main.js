9a39add9cafa346b65c72c00a7d5c14f1a7747c5
maruson08 <maruson08@naver.com>
2023-07-22 17:57:39 +0900

    index.html -> relative path fixed caategory pages merge to one page category.html

--- src/main.js
+++ src/main.js
@@ -1,15 +1,38 @@
 $(document).ready(function(){
-    category = new URLSearchParams(window.location.search).get('category');
-    console.log('1st check');
+    let category = new URLSearchParams(window.location.search).get('category');
+    $("logo").html(`<h1>${category}</h1>`);
     db.collection('Data').doc(category).get().then((result)=>{
         data = result.data()
         Object.keys(data).forEach((key)=>{
             let video = `
-            <h1>${key}</h1>
+            <div class='video'>
+            <h2>${key}</h2>
             <iframe width="560" height="315" src=${data[key]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
             `
             $('#container').append(video);
         })
         
-  })
+    })
+})
+$(document).ready(function(){
+    $("a").click((e)=>{
+        $('#container').html('');
+        let category = $(e.target).get(0).id;
+        console.log(category)
+        $("logo").html(`<h1>${category}</h1>`);
+        db.collection('Data').doc(category).get().then((result)=>{
+            data = result.data()
+            Object.keys(data).forEach((key)=>{
+                let video = `
+                <div class='video'>
+                <h2>${key}</h2>
+                <iframe width="560" height="315" src=${data[key]} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+                `
+                $('#container').append(video);
+            })
+            
+        })
+    })
 })
